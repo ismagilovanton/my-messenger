fd11ed7bfc215e2df7c5b5a86b4da377e886ac20
ismagilov_anton <oldvertu@gmail.com>
2025-05-04 15:46:02 +0700

    fix: removed comments

--- src/views/profile/profile.tmpl.ts
+++ src/views/profile/profile.tmpl.ts
@@ -1,15 +1,4 @@
-import Picture from "../../../public/icons/picture.svg";
-
-
-// {{> input-inline type="email" label="Почта" value="pochta@yandex.ru" name="email"}}
-// {{> input-inline type="text" label="Логин" value="ivanivanov" name="login"}}
-// {{> input-inline type="text" label="Имя" value="Иван" name="first_name"}}
-// {{> input-inline type="text" label="Фамилия" value="Иванов" name="second_name"}}
-// {{> input-inline type="text" label="Имя в чате" value="Иван" name="display_name"}}
-// {{> input-inline type="phone" label="Телефон" value="+7 (909) 967 30 30" name="phone"}}
-// {{> input-inline type="password" label="Старый пароль" value="12345678" name="oldPassword"}}
-// {{> input-inline type="password" label="Новый пароль" value="12345678" name="newPassword"}}
-
+import Picture from '../../../public/icons/picture.svg';
 
 export default `
 
