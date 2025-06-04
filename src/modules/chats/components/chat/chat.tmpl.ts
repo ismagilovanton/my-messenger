e4987b3a5065e24fc0995fb5d87b3a4321d7d194
ismagilov_anton <oldvertu@gmail.com>
2025-06-04 21:03:41 +0700

    fix: added displaying image of chat in list

--- src/modules/chats/components/chat/chat.tmpl.ts
+++ src/modules/chats/components/chat/chat.tmpl.ts
@@ -1,9 +1,18 @@
+import { API_ENDPOINT } from '../../../../framework/Fetch';
+
 export default `
 <div class="chat" id="chat-{{chat.id}}">
      <div class="chat__avatar">
-     <img src="{{chat.avatar}}">
-     </div>
-     
+    
+
+            {{#if chat.avatar}}
+                <img src="${API_ENDPOINT}/resources/{{chat.avatar}}" alt="{{chat.title}}" />
+            {{else}}
+                <div class="user-card__avatar-placeholder">
+                    {{chat.title.[0]}}
+                </div>
+            {{/if}}
+      </div>
      <div class="chat__info">
         <div class="chat__info--title">
             <div class="name">{{chat.title}}</div>
